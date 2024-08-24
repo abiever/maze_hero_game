2581ef822ad93373cee60eb92606a89459b47e85
Aaron Tandem <aaronlbiever@gmail.com>
2024-08-24 21:22:21 +0900

    Refactors MazeController to no longer display Start Game button after clicking and for the next level start automatically. This seems to have fixed the issue of the 2nd+ game level automatically refreshing

--- main.js
+++ main.js
@@ -6,7 +6,14 @@ var Maze, MazeGame;
 var makeMaze = (id, width, height) => {
     Maze = new FancyMazeBuilder(width, height);
     Maze.display(id);
-    MazeGame = new MazeController("maze", 5, 0, Maze.returnMazeBuilderArray(), Maze.getMonsters(), Maze.getUpperWarpSpot(), Maze.getLowerWarpSpot());
+    MazeGame = new MazeController(
+            "maze", 
+            999, //made Hero Level high for easier testing purposes
+            0, 
+            Maze.returnMazeBuilderArray(), 
+            Maze.getMonsters(), 
+            Maze.getUpperWarpSpot(), 
+            Maze.getLowerWarpSpot());
 }        
       
 makeMaze("maze_container", 11, 9);
